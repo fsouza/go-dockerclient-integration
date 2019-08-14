c668b71708ab99fe3fbeb80d63ba955e37048a34
Daniel Black <daniel@linux.ibm.com>
2019-08-14 18:23:56 +1000

    Expand to include manifest
    This pushes the :latest tag as a manifest to include multiple
    platforms.
    
    We expand to include a ppc64le architecture at the same time.
    
    For safety, docker login/push aren't enabled on pull requests
    or non-master branch. Environment variables are currently
    protected however this prevents failures on PRs.
    
    The purpose is to include provide a test for go-dockerclient
    for docker API 1.34 "Create an Image" tests that has platform
    as an argument.

---
+++ .travis-manifest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x -v
+if [ "$TRAVIS_EVENT_TYPE" != "pull_request" ] &&
+   [ "$TRAVIS_BRANCH" = "master" ]; then
+     mkdir -p ~/.docker
+     echo '{ "experimental" : "enabled" }' > ~/.docker/config.json
+     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+     docker manifest create  "${REPO}:${RELEASE}" "${REPO}:linux" "${REPO}:windows" "${REPO}:linux_ppc64le"
+     docker manifest inspect "${REPO}:${RELEASE}"
+     docker manifest push    "${REPO}:${RELEASE}"
+fi
